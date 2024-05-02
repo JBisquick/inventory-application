b0f8fb2fd8ea1d929495c8382a74eda55039395e
JBisquick <felipebirch@gmail.com>
2024-05-02 12:20:33 -0700

    Add controller skeleton

--- app.js
+++ app.js
@@ -1,8 +1,8 @@
-var createError = require('http-errors');
-var express = require('express');
-var path = require('path');
-var cookieParser = require('cookie-parser');
-var logger = require('morgan');
+const createError = require('http-errors');
+const express = require('express');
+const path = require('path');
+const cookieParser = require('cookie-parser');
+const logger = require('morgan');
 
 // Set up mongoose connection
 const mongoose = require("mongoose");
@@ -14,9 +14,9 @@ async function main() {
   await mongoose.connect(mongoDB);
 }
 
-var indexRouter = require('./routes/index');
+const indexRouter = require('./routes/index');
 
-var app = express();
+const app = express();
 
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
