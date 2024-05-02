63c1cf9347b12fbd03f744ecfb1b3fddb48fe7f1
JBisquick <felipebirch@gmail.com>
2024-05-02 14:07:05 -0700

    Add home page layout

--- controllers/itemController.js
+++ controllers/itemController.js
@@ -1,10 +1,23 @@
 const Item = require("../models/item");
+const Category = require("../models/category")
+
 const asyncHandler = require("express-async-handler");
 
 exports.index = asyncHandler(async (req, res, next) => {
-  res.send("NOT IMPLEMENTED: Site Home Page");
-});
+  const [
+    numItems,
+    numCategories,
+  ] = await Promise.all([
+    Item.countDocuments({}).exec(),
+    Category.countDocuments({}).exec(),
+  ]);
 
+  res.render("index", {
+    title: "Grocery Store Home",
+    item_count: numItems,
+    category_count: numCategories,
+  })
+});
 // Display list of all item.
 exports.item_list = asyncHandler(async (req, res, next) => {
   res.send("NOT IMPLEMENTED: Item list");
