b33f6fecadcd53836da130a73b5555fe9d5dc14f
JBisquick <felipebirch@gmail.com>
2024-05-06 13:47:36 -0700

    Add delete button to items

--- controllers/itemController.js
+++ controllers/itemController.js
@@ -112,12 +112,24 @@ exports.item_create_post = [
 
 // Display item delete form on GET.
 exports.item_delete_get = asyncHandler(async (req, res, next) => {
-  res.send("NOT IMPLEMENTED: Item delete GET");
+  const [item] = await Promise.all([
+    Item.findById(req.params.id).exec(),
+  ]);
+
+  if (item === null) {
+    res.redirect("/catalog/items");
+  }
+
+  res.render("item_delete", {
+    title: "Delete Item",
+    item: item,
+  });
 });
 
 // Handle item delete on POST.
 exports.item_delete_post = asyncHandler(async (req, res, next) => {
-  res.send("NOT IMPLEMENTED: Item delete POST");
+  await Item.findByIdAndDelete(req.body.itemid);
+  res.redirect("/catalog/items");
 });
 
 // Display item update form on GET.
